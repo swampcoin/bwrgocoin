7d03dc450fc5b8da5fbddee463760510c9441a73
mallman <michael@swampthing.net>
2019-09-11 18:32:11 +0000

    renames in share

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # create multiresolution windows icon
 #mainnet
-ICON_SRC=../../src/qt/res/icons/ucc.png
-ICON_DST=../../src/qt/res/icons/ucc.ico
-convert ${ICON_SRC} -resize 16x16 ucc-16.png
-convert ${ICON_SRC} -resize 32x32 ucc-32.png
-convert ${ICON_SRC} -resize 48x48 ucc-48.png
-convert ucc-16.png ucc-32.png ucc-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/nwo.png
+ICON_DST=../../src/qt/res/icons/nwo.ico
+convert ${ICON_SRC} -resize 16x16 nwo-16.png
+convert ${ICON_SRC} -resize 32x32 nwo-32.png
+convert ${ICON_SRC} -resize 48x48 nwo-48.png
+convert nwo-16.png nwo-32.png nwo-48.png ${ICON_DST}
 #testnet
-ICON_SRC=../../src/qt/res/icons/ucc_testnet.png
-ICON_DST=../../src/qt/res/icons/ucc_testnet.ico
-convert ${ICON_SRC} -resize 16x16 ucc-16.png
-convert ${ICON_SRC} -resize 32x32 ucc-32.png
-convert ${ICON_SRC} -resize 48x48 ucc-48.png
-convert ucc-16.png ucc-32.png ucc-48.png ${ICON_DST}
-rm ucc-16.png ucc-32.png ucc-48.png
+ICON_SRC=../../src/qt/res/icons/nwo_testnet.png
+ICON_DST=../../src/qt/res/icons/nwo_testnet.ico
+convert ${ICON_SRC} -resize 16x16 nwo-16.png
+convert ${ICON_SRC} -resize 32x32 nwo-32.png
+convert ${ICON_SRC} -resize 48x48 nwo-48.png
+convert nwo-16.png nwo-32.png nwo-48.png ${ICON_DST}
+rm nwo-16.png nwo-32.png nwo-48.png
